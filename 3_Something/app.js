2c8c91badf7e0f80e4b6dbf8b0218cc81f1c4253
MrErin <erinmeaker@gmail.com>
2019-01-02 20:54:57 -0600

    types

---
+++ 3_Something/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var myName = 'Erin';
+// myName = 23;
+var myAge = 2837;
+// myAge = 'two hundred';
+var hasHobbies = true;
+// hasHobbies = 1;
+// If you don't explicitly assign a type, it becomes 'any' type
+var myRealAge;
+myRealAge = 27;
+myRealAge = '27';
+var seriouslyMyAge;
+seriouslyMyAge = 313;
+var hobbies = ['sewing', 'quilting', 'world domination'];
+console.log(hobbies[0]);
---
+++ 3_Something/app.ts
@@ -0,0 +1,20 @@
+let myName = 'Erin';
+
+// myName = 23;
+
+let myAge = 2837;
+// myAge = 'two hundred';
+
+let hasHobbies = true;
+// hasHobbies = 1;
+
+// If you don't explicitly assign a type, it becomes 'any' type
+let myRealAge;
+myRealAge = 27;
+myRealAge = '27';
+
+let seriouslyMyAge: number;
+seriouslyMyAge = 313;
+
+let hobbies = ['sewing', 'quilting', 'world domination']
+console.log(hobbies[0])
