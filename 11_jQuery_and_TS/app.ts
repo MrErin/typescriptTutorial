b79f0e765931b469aa7045f8470ab4dd7e6a641d
Erin Meaker <erinmeaker@gmail.com>
2019-01-19 14:10:31 -0600

    finished 11

---
+++ 11_jQuery_and_TS/app.ts
@@ -0,0 +1,8 @@
+// this is the simplest way to make this work, but it's not great.
+// declare var $: any
+
+// it's better to install a definition file from definitelytyped via npm
+
+$("button").click(function() {
+  alert("button was clicked");
+});
